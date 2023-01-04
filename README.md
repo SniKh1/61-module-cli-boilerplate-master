56bf1d93fa8876148f1fdab886bd1e210cc6c11a
SniKh1 <32765117+SniKh1@users.noreply.github.com>
2023-01-04 16:53:10 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# cli-parent 
+
+## 功能介绍
+1. 易于使用的 cli-parent
+2. 帮助你快速高效的开发cli工具   
+
+## 安装
+```
+yarn
+```
+
+## 开发时运行   
+```
+node run start   
+node run demo 
+// ... others 
+```
+ 
---
+++ run.js
@@ -0,0 +1,6 @@
+ require('ts-node/register') 
+ process.chdir('C:\\Users\\hubiqing\\Desktop\\新建文件夹')
+ require('./src')
+     //tsconfig 帮助文档地址
+     //https://zhongsp.gitbooks.io/typescript-handbook/content/doc/handbook/tsconfig.json.html
+     //https://tslang.cn/docs/handbook/compiler-options.html
